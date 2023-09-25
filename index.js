487de2f39da501bc2adf9793212bcbf926408c93
abubakr98 <abubakrfazilov98@gmail.com>
2023-09-25 11:49:10 +0300

    Done work

---
+++ index.js
@@ -0,0 +1,17 @@
+function clickNumber(numberValue) {
+  document.getElementById("window").innerHTML += "" + numberValue;
+}
+
+function showResult(str) {
+  document.getElementById("window").innerHTML = eval(str);
+}
+
+function clearWindow(str) {
+  let array = str.split("");
+  array[array.length - 1] = "";
+  document.getElementById("window").innerHTML = array.join("");
+}
+
+function clearAll() {
+  document.getElementById("window").innerHTML = "";
+}
