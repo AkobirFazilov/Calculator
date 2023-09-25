d2b9cebd1cb41c8bfb00a5c1255de39c27abb4b6
Akobir Fazilov <akobirfazilov1@gmail.com>
2023-09-25 12:03:19 +0300

    Change GIT user

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function showResult(str) {
   document.getElementById("window").innerHTML = eval(str);
 }
 
-function clearWindow(str) {
+function clearWindowByOne(str) {
   let array = str.split("");
   array[array.length - 1] = "";
   document.getElementById("window").innerHTML = array.join("");
